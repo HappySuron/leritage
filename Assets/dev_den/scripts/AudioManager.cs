5531fa4da3fa6b6dd08384af4aa2b78388b89474
Erklio <den31052002@gmail.com>
2026-03-25 22:39:04 +0500

    added audio manager

---
+++ Assets/dev_den/scripts/AudioManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    [SerializeField] private AudioSource soundFXObject;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    }
+
+    public void PlaySoundFXClip(AudioClip[] audioClip, Transform spawnTransform, float volume)
+    {
+        int rand = Random.Range(0, audioClip.Length);
+
+        AudioSource audioSource = Instantiate(soundFXObject, spawnTransform.position, Quaternion.identity);
+
+        audioSource.clip = audioClip[rand];
+
+        audioSource.volume = volume;
+
+        audioSource.Play();
+
+        float clipLength = audioSource.clip.length;
+
+        Destroy(audioSource.gameObject, clipLength);
+    }
+}
