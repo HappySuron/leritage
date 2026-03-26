a86a06dbf49d8e7f5d7b657ea04198cadb285465
HappySuron <zaburdinslv@mail.ru>
2026-03-26 23:16:19 +0300

    commit cause that piece of shit it ruining

--- Assets/dev_hip/Scripts/RightHandCursor.cs
+++ Assets/dev_hip/Scripts/RightHandCursor.cs
@@ -1,33 +1,57 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class RightHandCursor : MonoBehaviour
+public class TwoHandCursor : MonoBehaviour
 {
-    public RectTransform handImage;
-    public Image handRenderer;
-    public Sprite[] open;  // по эпохам
-    public Sprite[] grab;  // по эпохам
+    [System.Serializable]
+    public class Hand
+    {
+        public RectTransform handImage;
+        public Image handRenderer;
+        public Sprite[] open;
+        public Sprite[] grab;
 
-    private int _epoch = 0;
+        [HideInInspector] public int epoch;
+    }
+
+    public Hand rightHand;
+    public Hand leftHand;
 
     void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Confined;
+
+        SetEpoch(0);
     }
 
     void Update()
     {
-        handImage.position = Input.mousePosition;
+        // Правая рука — всегда за мышью
+        rightHand.handImage.position = Input.mousePosition;
+
+        // Левая — например по кнопке Q
+        if (Input.GetKey(KeyCode.Q))
+        {
+            leftHand.handImage.position = Input.mousePosition;
+        }
     }
 
-    public void OnGrabStart() => handRenderer.sprite = grab[_epoch];
-    public void OnGrabEnd()   => handRenderer.sprite = open[_epoch];
+    // --- Правая ---
+    public void RightGrabStart() => rightHand.handRenderer.sprite = rightHand.grab[rightHand.epoch];
+    public void RightGrabEnd()   => rightHand.handRenderer.sprite = rightHand.open[rightHand.epoch];
+
+    // --- Левая ---
+    public void LeftGrabStart() => leftHand.handRenderer.sprite = leftHand.grab[leftHand.epoch];
+    public void LeftGrabEnd()   => leftHand.handRenderer.sprite = leftHand.open[leftHand.epoch];
 
     public void SetEpoch(int index)
     {
-        _epoch = index;
-        handRenderer.sprite = open[_epoch];
+        rightHand.epoch = index;
+        leftHand.epoch = index;
+
+        rightHand.handRenderer.sprite = rightHand.open[index];
+        leftHand.handRenderer.sprite = leftHand.open[index];
     }
 
     void OnApplicationFocus(bool focus) => Cursor.visible = !focus;
